6d9e20915a29468e6644eae002f91993263798b1
Corey Ray <coreyaray@gmail.com>
2015-09-20 13:28:51 -0400

    Update functions.php

--- production/functions.php
+++ production/functions.php
@@ -6,13 +6,20 @@
  * @copyright Copyright (C) 2015 ITE Solutions. All rights reserved.
  * @license GNU General Public License version 2 or later; see LICENSE.txt
  */
+ 
+ /******************************************************************************************/
+ 
+ if (!defined ('ERROR_FILE')) {
+     define ('ERROR_FILE', 'error.log');
+ }
+ 
 /**
  * Function to get the name of the class that called the current method or function
  * @return string Returns a string on success or boolean false on fail
  */
 function get_calling_class() {
-    $trace = debug_backtrace();
-    if (isset($trace[2])) {
+    $trace = debug_backtrace ();
+    if (isset ($trace[2])) {
         $class = $trace[2]['class'];
         if (stristr($class, '\\')) {
             $array = explode ('\\', $class);
@@ -22,20 +29,22 @@ function get_calling_class() {
     }
     return FALSE;
 }
+
 /**
  * Function to get the calling method or function
  * @access public
  * @return array_assoc Returns an associative array with the class as the key and the method or function as the value or false on failure
  */
 function get_calling_method() {
-    $trace = debug_backtrace();
-    if (isset($trace[1])) {
+    $trace = debug_backtrace ();
+    if (isset($trace [1])) {
         return array(
             $trace[1]['class'] => $trace[1]['function']
         );
     }
     return FALSE;
 }
+
 /**
  * Error handler
  */
@@ -43,13 +52,14 @@ function error_handler($code, $string, $file, $line, $context) {
     include 'error.php';
     return TRUE;
 }
+
 /**
  * Function to log errors to a log file
  * @access public
  * @param Exception $error The exception thrown or a string with a message to log
  */
 function log_error($error) {
-    if (is_a($error, 'Exception')) {
+    if (is_a ($error, 'Exception')) {
         $msg = 'An Exception was thrown';
     } elseif (is_string($error)) {
         $msg = $error;
@@ -57,8 +67,9 @@ function log_error($error) {
         $msg = "An error occured: unknown";
     }
     
-    error_log($msg . PHP_EOL, 3, 'error.log');
+    error_log ($msg . PHP_EOL, 3, ERROR_FILE);
 }
+
 /**
  * Autoloader function using namespaces
  * 
@@ -77,14 +88,16 @@ function autoload($class_name) {
  * @return boolean True on success, false on fail.
  */
 function file_rewrite($file, $data) {
-    $fp = fopen($file, 'w');
+    $fp = fopen ($file, 'w');
     if (!$fp) {
-        trigger_error('Unable to get file handle: ' . $file . '. Check folder permissions.');
+        trigger_error ('Unable to get file handle: ' . $file . '. Check folder permissions.');
     }
-    $start = microtime();
+    
+    // Give the system time to get a lock on error file
+    $start = microtime ();
     do {
-        $canWrite = flock($fp, LOCK_EX);
-        if(!$canWrite) {
+        $canWrite = flock ($fp, LOCK_EX);
+        if (!$canWrite) {
             usleep(100);
         }
     }
@@ -92,11 +105,13 @@ function file_rewrite($file, $data) {
     if (!$canWrite) {
         return FALSE;
     }
+    
     fwrite($fp, $data);
     flock($fp, LOCK_UN);
     fclose($fp);
     return TRUE;
 }
+
 /**
  * Check if an array is associative
  * If there is at least one string key, $array will be regarded as associative array
@@ -105,8 +120,9 @@ function file_rewrite($file, $data) {
  */
 function is_assoc($array) {
     if (!is_array($array)) { return FALSE; }
-    return (bool)count(array_filter(array_keys($array), 'is_string'));
+    return (bool) count(array_filter(array_keys($array), 'is_string'));
 }
+
 /**
  * Checks if an array is indexed
  * @param array $array The array to be checked
